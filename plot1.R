090de26679cd0997d0f8da6e4967e4eff83be9c9
valerioorfano <ingorfano@hotmail.com>
2014-05-09 10:56:17 +0200

    Course Project 1

--- plot1.R
+++ plot1.R
@@ -5,6 +5,6 @@ mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
 power<- read.csv.sql(myFile, sql=mySql, sep=";")
 power$newDate<-paste(power$Date,power$Time)
 power$newDate<-strptime(power$newDate,"%d/%m/%Y %H:%M:%S")
+png("plot1.png", width=480, height=480)
 hist(power$Global_active_power, col="red",xlab="Global Active Power (kilowatts)" , main="Global Active Power",bg = "transparent")
-dev.copy(png,file="plot1.png" ,width = 480, height = 480)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -5,11 +5,11 @@ mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
 power<- read.csv.sql(myFile, sql=mySql, sep=";")
 power$newDate<-paste(power$Date,power$Time)
 power$newDate<-strptime(power$newDate,"%d/%m/%Y %H:%M:%S")
+png("plot3.png", width=480, height=480)
 plot(power$newDate, power$Sub_metering_1, type="n",xlab="", ylab="Energy sub metering",bg = "transparent")
 points(power$newDate, power$Sub_metering_1, type="l", col="black")
 points(power$newDate, power$Sub_metering_2, type="l", col="red")
 points(power$newDate, power$Sub_metering_3, type="l", col="blue")
-legend("topright",lty=1,xjust = 0, yjust = 1,y.intersp=0.8,x.intersp=0.5,cex=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(png,file="plot3.png", width = 480, height = 480)
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
 
